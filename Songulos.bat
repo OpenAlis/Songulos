4a7655c8b05d26663aa627e42d6768702fafa649
OpenAlis <166360335+OpenAlis@users.noreply.github.com>
2024-04-08 00:03:23 -0500

    beta-v0.1

---
+++ Songulos.bat
@@ -0,0 +1,13 @@
+@echo off
+title Songulos v0.1
+color 0a
+
+echo Please wait...
+timeout > null 10
+cls
+echo Welcome To Songulos
+echo ------------------------
+echo [1] Play Songs
+echo [2] Find Songs
+echo [3] Exit
+pause
